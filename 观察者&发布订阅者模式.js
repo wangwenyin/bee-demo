590bd0fbac19b0059bc8b85a2274739558147e1b
wang.wenyin <wang.wenyin@wezhuiyi.com>
2023-03-23 18:55:46 +0800

    优化发布订阅模式

--- 观察者&发布订阅者模式.js
+++ 观察者&发布订阅者模式.js
@@ -39,33 +39,41 @@ sub.notity()
 // 发布订阅模式：就像孩子们在学校一样，有很多科目老师，每个科目老师就相当于一个发布者，一群孩子就相当于订阅者，每一个老师都是一个不同的实例函数，在发布任务和作业的时候都不会影响到其他人
 class Publisher {
   constructor() {
-    this.observers = {}
+    this.events = {}
+  }
+  
+  // 注册事件
+  on(type, fn) {
+    (this.events[type] || (this.events[type] = [])).push(fn)
   }
 
-  on(key, fn) {
-    if (!this.observers[key]) {
-      this.observers[key] = []
+  // 触发事件
+  emit(type, params) {
+    if (!this.events[type]) return;
+    this.events[type].forEach(fn => fn(params));
+  }
+
+  // 移除事件
+  off(type, fn) {
+    if (!arguments.length) {
+      // 移除所有
+      this.events = {}
+    } else if (arguments.length === 1) {
+      // 移除某一类
+      this.events[type] = []
+    } else if (arguments.length === 2) {
+      // 移除具体回调
+      if (!this.events[type]) return;
+      this.events[type] = this.events[type].filter(f => f !== fn);
     }
-    this.observers[key].push(fn)
-  }
-
-  emit(key, params) {
-    if (!this.observers[key]) return
-    // 执行
-    this.observers[key].forEach(fn => fn(params))
-  }
-
-  off(key) {
-    delete this.observers[key]
   }
 }
 
-const pub = new Publisher()
-pub.on('math', (sub) => {
-  console.log(`我正在上${ sub }课`)
-})
-pub.on('english', (sub) => {
-  console.log(`我正在上${ sub }课`)
-})
-pub.emit('math', '数学')
-pub.emit('english', '英语')
+const pub = new Publisher();
+const fn = function (params) {
+  console.log(`我正在上${params}课`)
+}
+pub.on('math', fn);
+pub.on('english', fn);
+pub.emit('math', '数学');
+pub.emit('english', '英语');
